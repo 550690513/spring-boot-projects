a8cf3c0af24d01dab138523b3787a27dba0ad9ea
Cheung <550690513@qq.com>
2018-06-21 20:01:23 +0800

    completed the code

---
+++ spring-boot-static-resources1/src/main/java/com/cheung/SpringBootStaticResources1Application.java
@@ -0,0 +1,12 @@
+package com.cheung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStaticResources1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStaticResources1Application.class, args);
+	}
+}
