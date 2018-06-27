89033fd306cc993e09f7ebf00a1e0001775703f0
Cheung <550690513@qq.com>
2018-06-27 11:24:10 +0800

    completed the code.

---
+++ spring-boot-view-freemarker/src/main/java/com/cheung/App.java
@@ -0,0 +1,17 @@
+package com.cheung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot启动类
+ *
+ * @author Cheung
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
